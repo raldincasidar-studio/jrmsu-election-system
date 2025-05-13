20df4cc1dd02d350b3755aec18a941622621691a
raldincasidar-studio <raldincasidar@gmail.com>
2025-05-13 13:02:00 +0800

    add credits to frontpage

--- pages/index.vue
+++ pages/index.vue
@@ -17,12 +17,41 @@
         | {{ isLoading ? 'Logging in...' : 'Login' }}
         svg(xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6")
             path(stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3")
+
+
+    marquee(direction="left" class="marquee" behavior="scroll" scrolldelay="2") 
+        | Copyright &copy; 2025
+        | Powered by CCS-Creatives Commitee. 
+        | Chairperson: Sheen Lee S. Edis
+        | President: Stefhanie Ann V. Batucan
+        | Vice-President: Ej A. Vinculado
+        | Secretary: Nesfhe Niña S. Magsanay
+        | Treasurer: Mischi Jeda J. Elumba
+        | Auditor: Peter Robert C. Ayono
+        | P.I.O.: Erl Yves C. Tagaro
+        | Lead Back-End Developer: Resalute, Gehan Lumantas
+        | Back-End Developers: Elumba, Robert Mayo Lomocso; Santiago, Renz Labadan; Hampac, Marklan Alforque; Ej A. Vinculado; Abang, Rusty Lloyd Acas; Edis, Sheen Lee Sotomayor
+        | Lead Front-End Developer: Raldin C. Disomimba
+        | Front-End Developers: Peter Robert C. Ayono; Erl Yves C. Tagaro; Ontoy, Jazrelle Tan; Binondo, Kenneth Bobby Divinagracia; Abang, Rusty Lloyd Acas; Edis, Sheen Lee Sotomayor
+        | Lead UI/UX Graphic Designer: Almosera, Earl Jarvy Naong
+        | UI/UX Graphic Designer: Mischi Jeda J. Elumba
 </template>
 
 <style lang="scss" scoped>
 
 
-
+marquee {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    padding: 5px;
+    background-color: #ead012;
+    color: black;
+    font-size: 15px;
+    z-index: 99;
+}
 
 .containerx {
     max-width: 500px;
