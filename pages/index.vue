3253915dc4f30feaa9fd53f36e9ce3431b44e30b
raldincasidar-studio <raldincasidar@gmail.com>
2025-05-14 10:11:21 +0800

    fix login system:

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,13 @@
     h1 JRMSU SSG Electronic Election Portal
     p Welcome to the JRMSU SSG Election Portal
 
+    //- Error Warning
     .warn
         svg(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6")
             path(fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clip-rule="evenodd")
         p 
-            b Notice (May 14, 2025 9:30 AM): 
-            | Login issues have been reported due to a problem with JRMSU-ARMS. The Creatives team is actively working to resolve the issue and restore the service.
-
-
+            b Notice (May 14, 2025 10:12 AM): 
+            | The Creatives team has resolved the issue with the login system. You may now login using your student ID and the default password (Last Name).
     .input-container
         label Student ID
         input(type="text" placeholder="Student ID: eg. 25-A-00000" v-model="student_id" :disabled="isLoading")
@@ -64,7 +63,7 @@ marquee {
 .warn {
     display: flex;
     align-items: center;
-    background-color: #ead012;
+    background-color: #12ea73;
     padding: 15px;
     border-radius: 10px;
 
@@ -122,7 +121,7 @@ async function login() {
     const { data, error } = await useMyFetch('Voter/Account/Login', {
         method: 'POST',
         body: {
-            Username: student_id.value, 
+            Student_ID: student_id.value, 
             Password: password.value,
             Semester: "2nd",
             School_Year: "2024-2025"
